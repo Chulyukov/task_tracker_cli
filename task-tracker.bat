29765bd8e26908159084dff1e90a33bd7202fe08
Chulyukov Nikita <nvchulyukov@alfabank.ru>
2026-09-07 11:12:15 +0300

    change charset

--- task-tracker.bat
+++ task-tracker.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 >nul
 mkdir out 2>nul
-javac -d out src\*.java
-java -cp out CLI %*
+javac -encoding UTF-8 -d out src\*.java
+java -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -cp out CLI %*
